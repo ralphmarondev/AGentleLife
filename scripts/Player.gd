e8bd7fbd634cbdadf14a26fd7cb8a3c002a16f43
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-02-13 14:30:53 +0800

    rotating player (node) by name

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,9 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	position = Vector2(100, 200)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += Vector2(1, 0) * 5
+	position += Vector2(1, 0) * 50 * delta
+	$"PlayerImage".rotation += 0.1 * delta
