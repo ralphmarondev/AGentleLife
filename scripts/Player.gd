f4f2f6679fc2d14d5ca42d392580fe8742cd6064
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-02-13 14:41:05 +0800

    moving player with my keyboard

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+@export var speed = 500
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	position = Vector2(100, 200)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position += Vector2(1, 0) * 50 * delta
-	$"PlayerImage".rotation += 0.1 * delta
+func _process(delta: float):
+	# -x (going left), +x (going right), -y (going up), +y (going down)
+	var direction = Input.get_vector("left", "right", "up", "down")
+	position += direction * speed * delta
